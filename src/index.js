3d6b0793fd3d65afab27f91ef091ef366543d528
CatiMai <77164603+CatiMai@users.noreply.github.com>
2021-01-22 21:55:34 +0100

    added the current date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+
+  let thisDate = date.getDate();
+  let year = date.getFullYear();
+  let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
+  let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let day = days[date.getDay()];
+  return `${day} ${thisDate}.${month}.${year} at ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
@@ -17,6 +52,9 @@ function displayTemperature(response) {
 
   let feltTempElement = document.querySelector("#felt-temp");
   feltTempElement.innerHTML = Math.round(response.data.main.feels_like);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let Key = "3aacdf70afc33650631ca99d10ae4afe";
